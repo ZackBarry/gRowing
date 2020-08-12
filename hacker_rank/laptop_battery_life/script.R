d4b2e4242f8b0abf62a085b903d972bf880b37da
ZackBarry <zackbarry13@gmail.com>
2020-08-11 21:31:36 -0700

    first (naive) attempt

---
+++ hacker_rank/laptop_battery_life/script.R
@@ -0,0 +1,9 @@
+input_time <- suppressWarnings(read.table("stdin", sep=" "))
+input_time <- as.double(input_time)
+
+train_data <- read.csv('trainingdata.txt', header = FALSE, sep = ',')
+colnames(train_data) <- c('x', 'y')
+model <- lm(y ~ x, data = train_data)
+
+prediction <- predict(model, data.frame(x = input_time))
+write(prediction, stdout())
