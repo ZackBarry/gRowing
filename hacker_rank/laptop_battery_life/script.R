60ccdd86002d279664471cdc29690947b0abac8c
ZackBarry <zackbarry13@gmail.com>
2020-08-22 15:35:53 -0700

    remove old project

--- hacker_rank/laptop_battery_life/script.R
+++ hacker_rank/laptop_battery_life/script.R
@@ -1,6 +1,8 @@
 input_time <- suppressWarnings(read.table("stdin", sep=" "))
 input_time <- as.double(input_time)
 
+input_time <- 0.09
+
 train_data <- read.csv('trainingdata.txt', header = FALSE, sep = ',')
 colnames(train_data) <- c('x', 'y')
 model <- lm(y ~ x, data = train_data)
