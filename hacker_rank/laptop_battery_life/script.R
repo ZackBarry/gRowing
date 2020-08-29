cd1f92db5964c67cdbb1438df46d7a8adf027a62
ZackBarry <zackbarry13@gmail.com>
2020-08-29 14:43:30 -0700

    finished

--- hacker_rank/laptop_battery_life/script.R
+++ hacker_rank/laptop_battery_life/script.R
@@ -1,11 +1,9 @@
 input_time <- suppressWarnings(read.table("stdin", sep=" "))
 input_time <- as.double(input_time)
 
-input_time <- 0.09
-
 train_data <- read.csv('trainingdata.txt', header = FALSE, sep = ',')
 colnames(train_data) <- c('x', 'y')
-model <- lm(y ~ x, data = train_data)
 
-prediction <- predict(model, data.frame(x = input_time))
+prediction = ifelse(input_time < 4, input_time * 2, 8)
+
 write(prediction, stdout())
