f92a8bf981b5252140f8236f66b0c36e4e000175
ZackBarry <zackbarry13@gmail.com>
2020-08-29 14:45:16 -0700

    remove old stuff

--- hacker_rank/laptop_battery_life/script.R
+++
@@ -1,9 +0,0 @@
-input_time <- suppressWarnings(read.table("stdin", sep=" "))
-input_time <- as.double(input_time)
-
-train_data <- read.csv('trainingdata.txt', header = FALSE, sep = ',')
-colnames(train_data) <- c('x', 'y')
-
-prediction = ifelse(input_time < 4, input_time * 2, 8)
-
-write(prediction, stdout())
